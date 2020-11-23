a42ead6be0deda90a5b3504f2c3e9776ce3e197c
Quinn Salas <qmsalas321@gmail.com>
2020-11-22 23:59:22 -0600

    feat: add init app and document

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import PropTypes from 'prop-types';
+import { AnimatePresence, motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
 
-export default MyApp
+const MyApp = ({ Component, pageProps, router }) => (
+  <AnimatePresence key={router.route}>
+    <motion.div
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0
+        },
+        pageAnimate: {
+          opacity: 1
+        }
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  </AnimatePresence>
+);
+
+MyApp.propTypes = {
+  Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+  router: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired
+};
+
+export default MyApp;
